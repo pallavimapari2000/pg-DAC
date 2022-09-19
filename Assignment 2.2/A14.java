63f5d06b66c957eb314f625e972a4a2018b37430
pallavimapari2000 <113437496+pallavimapari2000@users.noreply.github.com>
2022-09-19 08:40:18 +0530

    Add files via upload

---
+++ Assignment 2.2/A14.java
@@ -0,0 +1,23 @@
+class A14
+	{
+	public static void main(String args[])
+	{
+			for(int i=1;i<=3;i++)
+			{
+				for(int j=3;j<=i;j++)               
+				{
+				System.out.print(" ");  
+				}  
+			for(int j=3;j>=i;j--)               
+				{
+				System.out.print("*");  
+				}    
+				for(int j=3;j>=i;j--)               
+				{
+				System.out.print("*");  
+				}                                      
+			System.out.println();
+			}
+			
+	}
+	}
---
+++ Assignment 2.2/A15.java
@@ -0,0 +1,25 @@
+class A15
+
+	{
+	public static void main(String args[])
+	{
+		for (int i= 1; i<=4;i++)
+			{
+			System.out.print("*");
+			for(int j =3 ; j <= i ; j++)
+				{
+		
+				System.out.print(" ");
+			
+				}
+			if(i==2||i==3||i==4)
+				System.out.print("*");
+			
+			System.out.println();	
+	
+		
+			
+		}
+		System.out.print("*****");
+	}
+	}
